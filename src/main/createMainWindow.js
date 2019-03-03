3c815827efc98b43aaeb2077d0425d6d8e473b93
DaisukeSawada <qkkj18313@ares.eonet.ne.jp>
2019-03-03 21:49:29 +0900

    4-3まで完了

---
+++ src/main/createMainWindow.js
@@ -0,0 +1,17 @@
+import { BrowserWindow } from "electron";
+
+class MainWindow{
+    constructor(){
+        this.windows = new BrowserWindow({ width: 800, height: 600 });
+        this.windows.loadURL('file://${__dirname}/../../index.html');
+        this.windows.on("closed", ()=>{
+            this.windows = null;
+        });
+    }
+}
+
+function createMainWindow(){
+    return new MainWindow();
+}
+
+export default createMainWindow;
